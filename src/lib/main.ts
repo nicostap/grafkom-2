48c3de75740fc61bdef07db2c86009f26d60e159
Nico <nicoalexandrekurniawan@gmail.com>
2024-05-09 21:59:53 +0700

    Trying out env, might scrap later

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -9,39 +9,77 @@ export function renderMain() {
     renderer.shadowMap.enabled = true;
     document.body.appendChild(renderer.domElement);
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+
     // Clown
     let clown = new Clown(scene);
     // Victim
     let victim = new Victim(scene);
 
-    // Light
-    const dirLight = new THREE.DirectionalLight(0xffffff, 5);
-    dirLight.castShadow = true;
-    dirLight.shadow.camera.position.set(0, 0, 0);
-    dirLight.shadow.camera.top = 200;
-    dirLight.shadow.camera.bottom = -200;
-    dirLight.shadow.camera.left = -200;
-    dirLight.shadow.camera.right = 200;
-    dirLight.shadow.bias = -0.002;
-    scene.add(dirLight);
-    scene.add(dirLight.target);
+    // Map
+    let map = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load('./assets/cobblestone/7_diffuseOriginal.bmp');
+    const displacement = textureLoader.load('./assets/cobblestone/7_height.bmp');
+    const normal = textureLoader.load('./assets/cobblestone/7_normal.bmp');
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
+            if (map[i][j] == 0) {
+                // Wall
+                const mesh = new THREE.Mesh(
+                    new THREE.BoxGeometry(500, 500, 500, 50, 50),
+                    new THREE.MeshPhongMaterial({ color: 0x999999, map: texture, displacementMap: displacement, normalMap: normal })
+                );
+                mesh.position.set(i * 500 - 500, 250, j * 500 - 500);
+                scene.add(mesh);
+            }
+            else if (map[i][j] == 1) {
+                // Floor
+                let mesh = new THREE.Mesh(
+                    new THREE.PlaneGeometry(500, 500, 5, 5),
+                    new THREE.MeshPhongMaterial({ color: 0x999999, map: texture, displacementMap: displacement, normalMap: normal })
+                );
+                mesh.position.set(i * 500 - 500, 0, j * 500 - 500);
+                mesh.rotation.x = -Math.PI / 2;
+                mesh.receiveShadow = true;
+                scene.add(mesh);
+                // Ceiling
+                mesh = new THREE.Mesh(
+                    new THREE.PlaneGeometry(500, 500, 5, 5),
+                    new THREE.MeshPhongMaterial({ color: 0x999999, map: texture, displacementMap: displacement, normalMap: normal })
+                );
+                mesh.position.set(i * 500 - 500, 500, j * 500 - 500);
+                mesh.rotation.x = Math.PI / 2;
+                scene.add(mesh);
+            }
+        }
+    }
 
-    // Floor
-    const mesh = new THREE.Mesh(
-        new THREE.PlaneGeometry(2000, 2000),
-        new THREE.MeshPhongMaterial({ color: 0x999999 })
-    );
-    mesh.rotation.x = - Math.PI / 2;
-    mesh.receiveShadow = true;
-    scene.add(mesh);
+    // Light
+    const pLight = new THREE.PointLight(0xffffff, 100000);
+    pLight.castShadow = true;
+    // Set up shadow properties for the light
+    pLight.shadow.mapSize.width = 1024; // default
+    pLight.shadow.mapSize.height = 1024; // default
+    pLight.shadow.camera.near = 0.5; // default
+    pLight.shadow.camera.far = 450; // default
+    scene.add(pLight);
 
     // Input
     const input = new Input();
 
     // Helper
-    var helper = new THREE.CameraHelper( dirLight.shadow.camera );
+    var helper = new THREE.CameraHelper(pLight.shadow.camera);
     scene.add(helper);
 
     // Animation
@@ -57,12 +95,11 @@ export function renderMain() {
 
             // Clown
             clown.run(dt, input.keyPressed);
-            if(clown.object) {
-                dirLight.target.position.set(clown.object.position.x, 0, clown.object.position.z);
-                dirLight.position.set(clown.object.position.x, 400, clown.object.position.z);
+            if (clown.object) {
+                pLight.position.set(clown.object.position.x, 400, clown.object.position.z);
 
-                if(input.keyPressed['q']) cameraAngle -= 3 * Math.PI / 180;
-                if(input.keyPressed['e']) cameraAngle += 3 * Math.PI / 180;
+                if (input.keyPressed['q']) cameraAngle += 3 * Math.PI / 180;
+                if (input.keyPressed['e']) cameraAngle -= 3 * Math.PI / 180;
                 camera.position.set(
                     clown.object.position.x - 200 * Math.sin(cameraAngle),
                     200,
@@ -71,7 +108,7 @@ export function renderMain() {
                 camera.lookAt(clown.object.position.x, 150, clown.object.position.z);
             }
             // Victim
-            victim.mixer?.update(dt);
+            victim.run(dt, input.keyPressed);
 
             // Drawing scene
             renderer.render(scene, camera);
