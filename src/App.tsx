18afcdfba4d702471bb50ea34e5a797d0195a248
Nathan Adhitya <NathanAdhitya@users.noreply.github.com>
2024-07-04 00:39:18 +0700

    victim with animations

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import { Perf } from "r3f-perf";
 import "./App.css";
 import { Bar2 } from "./components/Bar2";
 import { Maze } from "./components/Maze";
-import { LB1 } from "./components/LB1";
-import { LB2 } from "./components/LB2";
-import { LB3 } from "./components/LB3";
-import { LB4 } from "./components/LB4";
 import { SpectatorControls } from "./components/SpectatorControls";
 import { City } from "./components/City";
+import { Victim } from "./components/Victim";
 
 function App() {
     return (
@@ -41,7 +38,6 @@ function App() {
                     position={[10000, 0, 0]}
                     scale={[60, 60, 60]}
                 />
-                <DrinkingVictim />
                 {/* Scene 2 - Mabok-mabok */}
                 <City
                     receiveShadow
@@ -49,6 +45,7 @@ function App() {
                     scale={[600, 600, 600]}
                     rotation={[0, Math.PI / 2, 0]}
                 />
+                <Victim position={[0, 10, -1000]} scale={[100, 100, 100]} />
             </Canvas>
         </KeyboardControls>
     );
