7c173b4d1e95da481e519d5e88ac1f6622749d36
NathanAdhitya <NathanAdhitya@users.noreply.github.com>
2024-07-04 11:01:49 +0700

    drinking man, faster scroll

--- src/components/BarScene.tsx
+++ src/components/BarScene.tsx
@@ -1,17 +1,18 @@
 import { Bar2 } from "./Bar2";
 import { Box } from "./Box";
-import { Victim } from "./Victim";
+import { Victim1 } from "./Victim1";
 
 export function BarScene(props: JSX.IntrinsicElements["group"]) {
     return (
         <group {...props}>
             <Bar2 receiveShadow />
             {/* mesh won't appear for some reason */}
-            {/* <Victim
-                position={[0, 2, 0]}
-                scale={[3, 3, 3]}
-                activeAction="DrunkWalking"
-            /> */}
+            <Victim1
+                position={[6.5, 0.5, -1]}
+                rotation={[0, Math.PI + Math.PI / 4, 0]}
+                scale={[2.5, 2.5, 2.5]}
+                activeAction="Drinking"
+            />
         </group>
     );
 }
